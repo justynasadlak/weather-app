7fa7a626ccb1a08955f35d562321438ec96aa9ab
Marta Kołodziej <marta.koodziej@gmail.com>
2019-04-10 17:38:48 +0200

    Changed import CSS and scripts in src/index.html

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
-import '../css/loading.css';
-import '../css/fontello.css';
-import '../css/layout.css';
-import '../css/look.css';
+import _css0 from '../css/loading.css';
+import _css1 from '../css/fontello.css';
+import _css2 from '../css/layout.css';
+import _css3 from '../css/look.css';
 import {setWeather} from './weather';
 setWeather('', true);  // default is geolocation
 
